b7343fff1fe05b3a40aa077cf9aa8ae6f44a085c
Desire Kaleba <desirekaleba@gmail.com>
2022-03-04 08:50:25 +0300

    Update db config

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '0975419237deska',
+    password: 'pass',
     database: 'sidehustle'
 })
 
